f2a69d86500bbba4ba5fc8dac49735fb32116439
Ryan Chua <ryan.s.chua@gmail.com>
2019-01-12 00:15:01 -0800

    return all media types related to search term

--- src/index.js
+++ src/index.js
@@ -1,30 +1,51 @@
 const express = require('express');
-const https = require('https');
+const request = require('request-promise');
 const app = express();
 const router = express.Router();
 const port = 4000;
-
 const endpoint = 'https://itunes.apple.com/search?term=';
 
 app.use('/api', router);
 
-router.get('/search', function(request, response) {
-  const term = request.query.term;
+const itunesAPI = {
+  getData: (term, types, catalog) => {
+    let type = types.pop();
 
-  https.get(`${endpoint}${term}`, (res) => {
-    let data = '';
+    return request({
+      'method': 'GET',
+      'uri': `${endpoint}${term}&media=${type}`,
+      'json': true,
+    })
+    .then((response) => {
+      catalog = catalog.concat(response.results);
 
-    res.on('data', (chunk) => {
-      data += chunk;
+      while (types.length > 0) {
+        return itunesAPI.getData(term, types, catalog);
+      }
+
+      return catalog;
     });
+  },
+}
 
-    res.on('end', () => {
-      response.send(JSON.parse(data));
-    });
-  })
-  .on('error', (err) => {
-    response.send(err.message);
-  });
+router.get('/search', function(req, response) {
+  const types = [
+    'movie',
+    'podcast',
+    'music',
+    'musicVideo',
+    'audiobook',
+    'shortFilm',
+    'tvShow',
+    'software',
+    'ebook'
+  ];
+  const term = req.query.term;
+  let catalog = [];
+
+  itunesAPI
+    .getData(term, types, catalog)
+    .then(data => response.send(data));
 });
 
 app.listen(port);
